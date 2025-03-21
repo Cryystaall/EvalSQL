390df337095da6acc5a8947d78a5184e49e944cc
Egor Khaybulov <egor.khaybulov@aphilia.io>
2025-03-21 12:21:41 +0100

    clean functions and fix errors

--- wv_functions.sql
+++ wv_functions.sql
@@ -36,30 +36,36 @@ CREATE FUNCTION random_position(@nb_rows INT, @nb_cols INT, @party_id INT)
             WHERE used_positions.row_pos = CAST(all_positions.row_pos AS VARCHAR(255))
               AND used_positions.col_pos = CAST(all_positions.col_pos AS VARCHAR(255))
         )
-        ORDER BY NEWID()
         );
+GO
 
 CREATE FUNCTION random_role(@party_id INT)
     RETURNS INT
 AS
-
+BEGIN
     DECLARE @wolf_id INT = 1;
     DECLARE @villager_id INT = 2;
     DECLARE @total_players INT;
     DECLARE @wolf_count INT;
     DECLARE @next_role INT;
+
     SELECT @total_players = COUNT(*)
     FROM players_in_parties
     WHERE id_party = @party_id;
+
     SELECT @wolf_count = COUNT(*)
     FROM players_in_parties
     WHERE id_party = @party_id AND id_role = @wolf_id;
+
     IF (@wolf_count < CEILING((@total_players + 1) / 3.0))
         SET @next_role = @wolf_id;
     ELSE
         SET @next_role = @villager_id;
+
     RETURN @next_role;
 END;
+GO
+
 CREATE FUNCTION get_the_winner(@party_id INT)
     RETURNS TABLE
         AS
@@ -90,3 +96,4 @@ CREATE FUNCTION get_the_winner(@party_id INT)
         )
         SELECT * FROM player_stats
         );
+GO
