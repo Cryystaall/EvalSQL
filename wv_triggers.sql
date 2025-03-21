1dbf0ad92bc5269b33a7e4fd12a26e47327ea3cf
Egor Khaybulov <egor.khaybulov@aphilia.io>
2025-03-21 12:53:53 +0100

    translate to sql server

--- wv_triggers.sql
+++ wv_triggers.sql
@@ -1,19 +1,34 @@
 -- Trigger 1
-
 CREATE TRIGGER after_turn_end
-AFTER UPDATE ON turns
-FOR EACH ROW
+    ON turns
+    AFTER UPDATE
+    AS
 BEGIN
-    IF NEW.end_time IS NOT NULL AND OLD.end_time IS NULL THEN
-        CALL COMPLETE_TOUR(NEW.id_turn, NEW.id_party);
-    END IF;
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+                 JOIN deleted d ON i.id_turn = d.id_turn
+        WHERE i.end_time IS NOT NULL AND d.end_time IS NULL
+    )
+        BEGIN
+            DECLARE @id_turn INT, @id_party INT;
+
+            SELECT TOP 1 @id_turn = i.id_turn, @id_party = i.id_party
+            FROM inserted i
+                     JOIN deleted d ON i.id_turn = d.id_turn
+            WHERE i.end_time IS NOT NULL AND d.end_time IS NULL;
+
+            EXEC COMPLETE_TOUR @TOUR_ID = @id_turn, @PARTY_ID = @id_party;
+        END
 END;
+GO
 
 -- Trigger 2
-
 CREATE TRIGGER after_player_insert
-AFTER INSERT ON players_in_parties
-FOR EACH ROW
+    ON players_in_parties
+    AFTER INSERT
+    AS
 BEGIN
-    CALL USERNAME_TO_LOWER();
+    EXEC USERNAME_TO_LOWER;
 END;
+GO
